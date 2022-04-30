af78946d95b1374f556cff6f1e88e64c7d86ea0c
mahesh53715 <98692045+mahesh53715@users.noreply.github.com>
2022-04-30 11:22:08 +0530

    Update App.java

--- src/main/java/com/mahesh/App.java
+++ src/main/java/com/mahesh/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World mahesh!" );
+        System.out.println( "Hello World mahesh kondiga!" );
     }
 }
